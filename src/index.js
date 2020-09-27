d04580dac67d41988933984677bfd7564100a416
unknown <mrmahmoudadell1996@gmail.com>
2020-09-27 15:16:50 -0700

    resolve links

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { WOW } from 'wowjs/dist/wow';
+// import { WOW } from 'wowjs/dist/wow';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // WoW - animation on scroll
-const wow = new WOW({
-  boxClass: 'wow', // animated element css class (default is wow)
-  animateClass: 'animated', // animation css class (default is animated)
-  offset: 0, // distance to the element when triggering the animation (default is 0)
-  mobile: true, // trigger animations on mobile devices (default is true)
-  live: true, // act on asynchronously loaded content (default is true)
+// const wow = new WOW({
+//   boxClass: 'wow', // animated element css class (default is wow)
+//   animateClass: 'animated', // animation css class (default is animated)
+//   offset: 0, // distance to the element when triggering the animation (default is 0)
+//   mobile: true, // trigger animations on mobile devices (default is true)
+//   live: true, // act on asynchronously loaded content (default is true)
 
-  scrollContainer: null, // optional scroll container selector, otherwise use window
-});
-wow.init();
+//   scrollContainer: null, // optional scroll container selector, otherwise use window
+// });
+// wow.init();
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -22,7 +21,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
